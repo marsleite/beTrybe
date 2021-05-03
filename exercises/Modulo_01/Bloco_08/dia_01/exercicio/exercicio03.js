302ae08c4112fb36054ced642a9796564a5b5664
marsleite <marsleite@gmail.com>
2021-05-03 08:50:56 -0300

    continuando o exercicio bonus

---
+++ exercises/Modulo_01/Bloco_08/dia_01/exercicio/exercicio03.js
@@ -0,0 +1,67 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+  
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+  
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+  
+const battleMembers = { mage, warrior, dragon };
+
+const dragonAttack = () => {
+  const minDmg = 15;
+  const { strength } = dragon;
+  const dmgDragon = Math.floor(Math.random() * (strength - minDmg) + minDmg);
+  return dmgDragon;
+}
+
+const warriorAttack = () => {
+  const { strength } = warrior;
+  const { weaponDmg } = warrior
+  const maxDmg = strength * weaponDmg;
+  const dmgWarrior = Math.floor(Math.random() * (maxDmg - strength) + strength);
+  return dmgWarrior;
+}
+
+const mageAttack = () => {
+  const mageMana = mage.mana;
+  const minDmg = mage.intelligence;
+  const maxDmg = minDmg * 2;
+  const turnsStats = {
+    manaSpent: 0,
+    demageDealt: 'Não possui mana suficiente',
+  };
+
+  if (mageMana > 15) {
+    const mageDmg = Math.floor(Math.random() * (maxDmg - minDmg) + minDmg);
+    turnsStats.manaSpent = 15;
+    turnsStats.demageDealt = mageDmg;
+    return turnsStats
+  }
+  return turnsStats;
+}
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack();
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+};
+gameActions.warriorTurn(warriorAttack);
+
+
+
+
