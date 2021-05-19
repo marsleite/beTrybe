7401dc49b05e29a881aaec96fdc16701240e8420
marsleite <marsleite@gmail.com>
2021-05-19 19:01:52 -0300

    exercicio 01

---
+++ exercises/Modulo_01/Bloco_10/Dia_02/exercicio01.test.js
@@ -0,0 +1,12 @@
+const { expect, it } = require('@jest/globals');
+
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+};
+
+it(`uppercase 'test' to equal 'TEST'`, (done) => {
+  uppercase('test', (str) => {
+    expect(str).toBe('TEST');
+    done();
+  })
+})
