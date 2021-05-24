b492393bc9a6e3e3dc6a5af6b7cacc13a3e564c3
marsleite <marsleite@gmail.com>
2021-05-24 15:41:49 -0300

    primeiro teste com o hello word react

---
+++ exercises/Modulo_02/Bloco_11/Dia_01/fixacao_conteudo/testando-meu-computador/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import helloWords from './helloWord'
+
+function App() {
+  return (
+    <helloWords />
+  );
+}
+
+export default App;
---
+++ exercises/Modulo_02/Bloco_11/Dia_01/fixacao_conteudo/testando-meu-computador/src/helloWord.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class helloWords extends React.Component {
+  render() {
+    return <span>Hello Worlds Nice!</span>
+  };
+}
+
+export default helloWords;
