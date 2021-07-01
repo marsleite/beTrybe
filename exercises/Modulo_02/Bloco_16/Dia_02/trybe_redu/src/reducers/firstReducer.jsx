0a4c1046b3b4b026b9ba987eb0b933c49244c445
Marcelo <marsleite@gmail.com>
2021-07-01 10:49:08 -0300

    criação da action

--- exercises/Modulo_02/Bloco_16/Dia_02/trybe_redu/src/reducers/firstReducer.jsx
+++ exercises/Modulo_02/Bloco_16/Dia_02/trybe_redu/src/reducers/firstReducer.jsx
@@ -1,7 +1,12 @@
-const INITIAL_STATE = 0;
+const INITIAL_STATE = {
+  state: '',
+};
 
 export default function firstReducer(state = INITIAL_STATE, action) {
-  if (action.type) {
-    return state
+  switch (action.type) {
+    case 'NEW_ACTION':
+      return { state: action.state };
+    default:
+      return state
   }
 }
