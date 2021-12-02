de03c1782c5c9341f5e7d491a17131fabb9d5599
Marcelo <marsleite@gmail.com>
2021-12-02 15:50:54 -0300

    primeira parte do exercicio de fixacao

---
+++ exercises/Modulo_03/bloco_30/dia_02/fixacao/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuario conectado. ID: ${socket.id}`);
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
