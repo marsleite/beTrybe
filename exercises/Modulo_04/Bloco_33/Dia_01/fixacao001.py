866a8de6ecba4d96e8aea03da761b92b943f90c4
Marcelo <marsleite@gmail.com>
2022-01-25 14:59:37 -0300

    fixacao exercicio

--- exercises/Modulo_04/Bloco_33/Dia_01/fixacao001.py
+++ exercises/Modulo_04/Bloco_33/Dia_01/fixacao001.py
@@ -3,3 +3,10 @@ class User:
         self.name = name
         self.email = email
         self.password = password
+
+
+meu_user = User("Marcelo Leite", "marsleite@gmail.com", "123456212")
+print(meu_user)
+print(meu_user.name)
+print(meu_user.email)
+print(meu_user.password)
